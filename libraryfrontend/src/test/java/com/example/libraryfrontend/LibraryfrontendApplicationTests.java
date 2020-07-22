fe5004a1677adb807551f7373e30242f551a8bf8
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2020-07-22 23:38:04 +0530

    Initial commit

---
+++ libraryfrontend/src/test/java/com/example/libraryfrontend/LibraryfrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryfrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
