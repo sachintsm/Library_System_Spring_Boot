8d3274e554054a0e6a89cfea0263ffe9b589c381
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2020-08-02 22:27:27 +0530

    Commit

--- librarysystem/src/main/java/com/librarysystem/librarysystem/services/impl/BookServiceImpl.java
+++ librarysystem/src/main/java/com/librarysystem/librarysystem/services/impl/BookServiceImpl.java
@@ -4,8 +4,9 @@ import com.librarysystem.librarysystem.domain.BooksDomain;
 import com.librarysystem.librarysystem.repositories.BooksRepository;
 import com.librarysystem.librarysystem.services.BookServices;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,20 +24,29 @@ public class BookServiceImpl implements BookServices {
     }
 
     @Override
-    public BooksDomain save(BooksDomain bookdata) {
+    public BooksDomain save(BooksDomain bookdata)
+    {
         return booksRepository.save(bookdata);
     }
 
     @Override
-    public String deleteById(Integer id) {
-        String msg = null;
-        booksRepository.deleteById(id);
-        msg = "Successfully Deleted!";
-        return msg;
+    public Map<String, String> deleteById(Integer id) {
+        String msg;
+        if(booksRepository.findById(id).isPresent()){
+            booksRepository.deleteById(id);
+            msg = "Successfully Deleted!";
+        }else{
+            msg = "Deleted Not Successful!";
+        }
+        return Collections.singletonMap("msg", msg);
     }
 
     @Override
-    public Optional<BooksDomain> findById(Integer id) {
-        return booksRepository.findById(id);
+    public String findById(Integer id) {
+        if(booksRepository.findById(id).isPresent()){
+            return String.valueOf(booksRepository.findById(id));
+        }else{
+            return ("Book Not Found!");
+        }
     }
 }
